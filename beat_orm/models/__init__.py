8092eb1e32ff45880ccf3e5daf0efd03f26f11f5
Ningzislay <ningzi0610@gmail.com>
2020-02-20 17:56:56 +0800

    添加获取json模型的接口

--- beat_orm/models/__init__.py
+++ beat_orm/models/__init__.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 import sqlalchemy
 import orm
 
@@ -12,3 +14,21 @@ class BaseOrmModel(orm.Model):
     id = orm.Integer(primary_key=True)
     created_datetime = orm.DateTime()
     updated_datetime = orm.DateTime()
+
+    def get_information(self, *fields, **kwargs) -> Dict[str, Any]:
+        """
+        一个获取 json 数据的接口
+        :param fields: 要获取的字段名
+        :param kwargs: json 中字段名需要更换的时候使用
+
+        usage:
+            >>> BaseOrmModel().get_information("id", createdDatetime="created_datetime")
+        """
+        res = dict()
+        for field in fields:
+            if hasattr(self, field):
+                res[field] = getattr(self, field)
+        for key, value in kwargs.items():
+            if hasattr(self, value):
+                res[key] = getattr(self, value)
+        return res
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="beat_orm",
-    version="0.0.2",
+    version="0.0.3",
     author="Ningzi",
     author_email="ningzi0610@gmail.com",
     description="beat's orm",
