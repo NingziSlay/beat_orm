caf5de7cb91dfa7e143e5c26f609dd36034cada4
Ningzislay <ningzi0610@gmail.com>
2020-02-20 17:13:00 +0800

    目录结构修改

--- beat_orm/__init__.py
+++ beat_orm/__init__.py
@@ -1,11 +1,52 @@
+import databases
+import sqlalchemy
+
 from .models import BaseOrmModel, metadata
-from . import db_tools
 
 
-def init_orm(db_url, debug=False):
+__all__ = ["init_orm", "session"]
+
+
+session = None
+
+
+def init_orm(db_url: str, debug=False) -> None:
+    """
+    初始化数据库映射，确保要在项目启动之前调用
+    :param db_url: url
+    :param debug: default = False
+    :return: None
+    """
     # 创建数据库引擎
-    engine = db_tools.get_engine(db_url, debug=debug)
+    engine = __get_engine(db_url, debug=debug)
     metadata.create_all(engine)
     # 创建session
-    session = db_tools.get_session(engine.url)
+    global session
+    session = __get_session(engine.url)
     BaseOrmModel.__database__ = session
+
+
+__session = None
+__engine = None
+
+
+def __get_session(url):
+    global __session
+    if not __session:
+        __session = databases.Database(url)
+    return __session
+
+
+def __get_engine(url, debug=False):
+    global __engine
+    if not __engine:
+        __engine = sqlalchemy.create_engine(
+            url,
+            echo=debug,
+            echo_pool=True,
+            pool_recycle=3600,
+            pool_size=100,
+            max_overflow=200,
+            pool_timeout=300
+        )
+    return __engine
