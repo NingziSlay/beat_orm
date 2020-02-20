9a25e2bb1eaa3a11a5f57de6f3cf5677ab551da9
Ningzislay <ningzi0610@gmail.com>
2020-02-20 16:58:43 +0800

    修改初始化逻辑

--- beat_orm/__init__.py
+++ beat_orm/__init__.py
@@ -1,8 +1,11 @@
 from .models import BaseOrmModel, metadata
-from .db_tools import get_session
+from . import db_tools
 
 
-def init_orm(engine):
-    session = get_session(engine)
+def init_orm(db_url, debug=False):
+    # 创建数据库引擎
+    engine = db_tools.get_engine(db_url, debug=debug)
+    metadata.create_all(engine)
+    # 创建session
+    session = db_tools.get_session(engine.url)
     BaseOrmModel.__database__ = session
-    return session
--- beat_orm/db_tools/__init__.py
+++ beat_orm/db_tools/__init__.py
@@ -1,17 +1,31 @@
+"""数据库相关接口"""
 import databases
+import sqlalchemy
 
+__all__ = ["get_engine", "get_session", "session"]
 
-__all__ = ["get_session", "session"]
-
-
+_engine = None
 _session = None
 session = _session
 
 
-def get_session(engine):
-    """获取数据库session
-    确保项目启动时一定运行！"""
+def get_session(url):
     global _session
     if not _session:
-        _session = databases.Database(engine)
+        _session = databases.Database(url)
     return _session
+
+
+def get_engine(url, debug=False):
+    global _engine
+    if not _engine:
+        _engine = sqlalchemy.create_engine(
+            url,
+            echo=debug,
+            echo_pool=True,
+            pool_recycle=3600,
+            pool_size=100,
+            max_overflow=200,
+            pool_timeout=300
+        )
+    return _engine
